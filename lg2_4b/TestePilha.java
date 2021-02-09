aea767bfd856015b3f75ed958a36b6ffb8052228
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-02-09 17:57:35 -0300

    FILA E PILHA

---
+++ lg2_4b/TestePilha.java
@@ -0,0 +1,39 @@
+package lg2_4b;
+
+import java.util.Stack;
+
+public class TestePilha {
+
+	public static void main(String[] args) {
+		
+		Pilha pilha = new Pilha();
+		
+		pilha.push("Maurício");
+		System.out.println(pilha);
+		
+		pilha.push("Guilherme");
+		System.out.println(pilha);
+		
+		String r1 = pilha.pop();
+		System.out.println(r1);
+		
+		String r2 = pilha.pop();
+		System.out.println(r2);
+		
+		System.out.println(pilha.vazia()); 
+		pilha.push("Marcelo");
+		System.out.println(pilha.vazia()); 
+		
+		Stack <String> stack = new Stack<String>();
+		
+		stack.push("Maurício");
+		stack.push("Guilherme");
+		
+		System.out.println(stack);
+		System.out.println(stack.pop());
+		System.out.println(stack);
+		
+		String nome = stack.peek();
+		System.out.println(nome);		
+	}
+}
