1d66cdf3fa04070f037639e003c98c60213310b4
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-02-09 17:53:18 -0300

    Árvore

---
+++ MainArvore.java
@@ -0,0 +1,26 @@
+package lg2_4b;
+
+public class MainArvore {
+	
+	public static void main(String[] args) {
+		Arvore<Integer> arvore = new Arvore<Integer>();
+		arvore.adicionar(10);
+		arvore.adicionar(8);
+		arvore.adicionar(5);
+		arvore.adicionar(9);
+        arvore.adicionar(7);
+        arvore.adicionar(18);
+        arvore.adicionar(13);
+        arvore.adicionar(20);
+        System.out.println("Em ordem:");
+		arvore.emOrdem(arvore.getRaiz());
+		
+		 	System.out.println("\nPré ordem:");
+	        arvore.preOrdem(arvore.getRaiz());
+
+	        System.out.println("\nPós ordem:");
+	        arvore.posOrdem(arvore.getRaiz());
+		
+	}
+
+}
