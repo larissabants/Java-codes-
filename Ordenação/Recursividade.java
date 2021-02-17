7fdb344f63e0373078025cf07e9922f60d4a5887
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-02-17 19:18:39 -0300

    Add files via upload

---
+++ Ordenação/Recursividade.java
@@ -0,0 +1,31 @@
+package lg2_4b;
+
+public class Recursividade {
+	
+	public static int fatorialNaoRecursivo (int num) {
+		
+		if (num == 0) {
+			return 1;
+		}
+		
+		int total = 1;
+		for (int i = num; i > 1; i--) {
+			total *= i;
+		}
+		
+		return total;
+	}
+	
+	public static int fatorial (int num) {
+		
+		if (num == 0) {
+			return 1;
+		}
+		
+		return num * fatorial(num-1);
+	}
+
+}
+
+
+
---
+++ Ordenação/TesteRecursividade_Calculadora.java
@@ -0,0 +1,15 @@
+package lg2_4b;
+
+public class TesteRecursividade_Calculadora {
+
+		public static void main(String[] args) {
+		
+		int fatorialNR = Recursividade.fatorialNaoRecursivo(5);
+		System.out.println(fatorialNR);
+		
+		int fatorialR = Recursividade.fatorial(5);
+		System.out.println(fatorialR);
+		
+	}
+
+}
